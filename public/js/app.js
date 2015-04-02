296f8e994ede0f0c56e0f8cb9d8367710060f8c1
pehaaada <adam@pehas.com>
2015-04-02 09:20:35 -0500

    UI Router Added

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,25 @@
-angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ui.router']);
 // public/js/appRoutes.js
+
+myApp.run(function() {
+
+});
+
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/home");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('user', {
+            url: "/user",
+            templateUrl: "views/user.html"
+        })
+        .state('home', {
+            url: "/home",
+            templateUrl: "views/home.html"
+        })
+
+});
